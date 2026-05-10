f947b2bcfb26cad08c2f4d36ccfc9ed5d2d547d4
Villoh <mikelvillotatic@gmail.com>
2026-05-10 21:40:58 +0200

    fix: change default port from 7890 to 8317

--- Services/AppSettings.cs
+++ Services/AppSettings.cs
@@ -5,7 +5,7 @@ namespace TunnelAgent.Services;
 
 public sealed class AppSettings
 {
-    public int Port { get; set; } = 7890;
+    public int Port { get; set; } = 8317;
     public string BindAddress { get; set; } = "127.0.0.1";
     public bool LaunchAtLogin { get; set; } = true;
     public bool IsDark { get; set; }
