8ae766809eca80404d62feaf0eafaf280ddf757a
Villoh <mikelvillotatic@gmail.com>
2026-05-10 21:14:47 +0200

    feat: add EngineState enum, remove StubProxyServer

--- ViewModels/Enums.cs
+++ ViewModels/Enums.cs
@@ -1,5 +1,8 @@
+// ViewModels/Enums.cs
 namespace TunnelAgent.ViewModels;
 
 public enum ServerState { Stopped, Starting, Running, Error }
 
 public enum SectionKey { Providers, Agents, Activity, Configuration }
+
+public enum EngineState { NotInstalled, Downloading, Installing, Stopped, Starting, Running, Error }
